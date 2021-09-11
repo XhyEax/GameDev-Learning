12ffbb70ce65b8def0645b6c9d9b25d8f7dd534b
XhyEax <xhyeax@qq.com>
2021-09-11 20:06:12 +0800

    update string class

--- Week-2/2-custom-string-class.cpp
+++ Week-2/2-custom-string-class.cpp
@@ -1,14 +1,14 @@
 /*
  * @Author: Xhy
  * @Date: 2021-09-11 11:26:40
- * @LastEditTime: 2021-09-11 16:04:22
+ * @LastEditTime: 2021-09-11 20:02:30
  * @Description: 
  */
 #include <iostream>
 #include <cstring>
 using namespace std;
 
-char *global_char_pointer = "char_pointer";
+char *global_char_pointer = (char *)"char_pointer";
 char global_char_array[20] = {"char_array"};
 
 class String
@@ -22,11 +22,26 @@ private:
         strcpy(string_char_arr, char_pointer);
     }
 
+    String concatWithCharPointer(const char *char_pointer)
+    {
+        int n = strlen(string_char_arr);
+        int m = strlen(char_pointer);
+        char *tmp = new char[n + m + 1];
+        memcpy(tmp, string_char_arr, n);
+        memcpy(tmp + n, char_pointer, m);
+        // 末尾添加 \0
+        tmp[n + m] = 0;
+        String res = String(tmp);
+        delete[] tmp;
+        return res;
+    }
+
 public:
     String()
     {
         string_char_arr = new char[2];
-        strcpy(string_char_arr, "0");
+        string_char_arr[0] = '0';
+        string_char_arr[1] = 0;
     }
 
     String(const char *char_pointer)
@@ -39,12 +54,22 @@ public:
         copyFromCharPointer(copyObj.string_char_arr);
     }
 
-    String &operator=(const String &copyObj)
+    String &operator=(const String &obj)
     {
         // free old array
         delete[] string_char_arr;
-        copyFromCharPointer(copyObj.string_char_arr);
+        copyFromCharPointer(obj.string_char_arr);
         return *this;
+    }
+
+    String operator+(const char *char_pointer)
+    {
+        return concatWithCharPointer(char_pointer);
+    }
+
+    String operator+(const String &obj)
+    {
+        return concatWithCharPointer(obj.string_char_arr);
     }
 
     friend ostream &operator<<(ostream &os, const String &obj);
@@ -63,13 +88,19 @@ ostream &operator<<(ostream &os, const String &obj)
 
 int main()
 {
-    String obj1 = String();
+    String obj0 = String();
+    cout << "obj0: " << obj0;
+    String obj1 = String(global_char_array);
     cout << "obj1: " << obj1;
     String obj2 = String(global_char_pointer);
     cout << "obj2: " << obj2;
-    String obj3 = String(global_char_array);
+    // 拷贝
+    String obj3(obj2);
     cout << "obj3: " << obj3;
-    obj1 = obj3;
+    // 加号、赋值
+    obj1 = obj2 + obj3;
+    cout << "obj1: " << obj1;
+    obj1 = obj1 + "1";
     cout << "obj1: " << obj1;
     return 0;
 }
--- Week-2/4-c-style-tetris.cpp
+++ Week-2/4-c-style-tetris.cpp
@@ -1,12 +1,54 @@
 /*
  * @Author: Xhy
  * @Date: 2021-09-11 11:27:43
- * @LastEditTime: 2021-09-11 16:05:41
+ * @LastEditTime: 2021-09-11 16:29:15
  * @Description: 
  */
 #include <iostream>
 using namespace std;
 
+const int HEIGHT = 10;
+const int WIDTH = 10;
+
+int graph[HEIGHT][WIDTH];
+
+void print_graph()
+{
+    for (int i = 0; i < WIDTH + 2; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        printf("*");
+        for (int j = 0; j < WIDTH; j++)
+        {
+            if (graph[i][j])
+            {
+                printf("■");
+            }
+            else
+            {
+                printf("_");
+            }
+        }
+        printf("*\n");
+    }
+
+    for (int i = 0; i < WIDTH + 2; i++)
+    {
+        printf("*");
+    }
+    printf("\n");
+}
+
 int main()
 {
+    graph[2][3] = 1;
+    graph[2][2] = 1;
+    graph[3][2] = 1;
+    print_graph();
+    return 0;
 }
